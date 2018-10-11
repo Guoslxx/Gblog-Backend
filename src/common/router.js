1ba50ba16abec3a5dda37385e5ee9f548a71d718
guoshilong <827005741@qq.com>
2018-10-11 17:46:53 +0800

    save

--- src/common/router.js
+++ src/common/router.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import Article from '../pages/article';
 import Dashboard from '../pages/dashboard';
+import PageLayout from '@layouts/PageLayout';
 
 const RouterMap = () => (
     <Switch>
+        <Route path="/" component={PageLayout}>
+            <Route path="dashboard" component={Dashboard} onEnter={() => { console.log('enter') }} />
+        </Route>
+        {/* <Route path="/" /> */}
+        {/* <Route path="/dashboard" component={Dashboard} onEnter={()=>{console.log('enter')}}/>
         <Route path="/article" component={Article} exact/>
-        <Route path="/dashboard" component={Dashboard} />
         <Redirect from="/" to="/dashboard" exact={true} />
-        <Route render={()=>(<h1>404</h1>)}></Route>
+        <Route render={()=>(<h1>404</h1>)}></Route> */}
     </Switch>
 );
 
