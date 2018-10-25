a2dd3d9eb6ed3c95e6832d8e52ed72befd6713bb
Guoslxx <mr.guosl@qq.com>
2018-10-25 19:38:04 +0800

    git config core.ignorecase false设置文件夹大小写敏感

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageLayout from '@layouts/PageLayout';
+
+class Dashboard extends React.PureComponent{
+    render(){
+        return (
+            <PageLayout>
+                <h1>控制面板</h1>
+            </PageLayout>
+        )
+    }
+}
+
+export default Dashboard;
